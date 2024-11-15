c4bfaac7c8e08718e639fd9112b3ac07c156f774
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-11-14 21:05:01 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Components/Header/header"
+
+function App() {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,11 @@
+import S from "./header.module.scss"
+
+function Header() {
+    return(
+        <Header className={S.inicio}>
+            ola
+        </Header>
+    )
+}
+
+export default Header
