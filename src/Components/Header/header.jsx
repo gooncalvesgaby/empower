24a8c3f9790c209cbe96390a9b82a0f95bd6af71
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-11-19 18:43:08 -0300

    ajuste header

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,36 +1,57 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Inicio from '../Pages/inicio/Inicio'
+import LivrosDoados from '../Pages/livrosDoados/Doados'
+import QueroDoar from '../Pages/queroDoar/Doar'
 import S from "./header.module.scss"
 import Livro from "./livro.png"
 import Estudo from "./estudo.png"
 
 function Header() {
     return(
-        <div>
-            <div className={S.inicio}>
-                <div className={S.box1}>
-                    <img className={S.livro} src={Livro} alt="imagem de um livro" />
-                    <h1 className={S.titulo}>Livros Vai Na Web</h1>
-                </div>
+        <BrowserRouter>
+            <header>
+                <section className={S.inicio}>
+                    <div className={S.box1}>
+                        <img className={S.livro} src={Livro} alt="imagem de um livro" />
+                        <h1 className={S.titulo}>Livros Vai Na Web</h1>
+                    </div>
 
-                <div className={S.box2}>
-                    <li className={S.lista}>
-                        <ol>Início</ol>
-                        <ol>Livros Doados</ol>
-                        <ol>Quero Doar</ol>
-                    </li>
-                </div>
-                <div>
-                    <input className={S.input} type="text" placeholder="O que você procura?" />
-                </div>
-            </div>
-            <div>
-                <img className={S.estudos} src={Estudo} alt="livros" />
-                <h2>VENHA FAZER PARTE <br />
-                DA MAIOR REDE DE <br />
-                DOAÇÃO</h2>
-            </div>
-        </div>
-
-
+                    <nav className={S.box2}>
+                        <ul>
+                            <li>
+                            <Link className={S.listas} to="/">
+                                Início
+                            </Link>
+                            </li>
+                            <li>
+                            <Link className={S.listas} to="/livrosdoados">
+                                Livros Doados
+                            </Link>
+                            </li>
+                            <li>
+                            <Link className={S.listas} to="/querodoar">
+                                Quero Doar
+                            </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <section>
+                        <input className={S.input} type="text" placeholder="O que você procura?" />
+                    </section>
+                </section>
+                <aside>
+                    <img className={S.estudos} src={Estudo} alt="livros" />
+                    <h2>VENHA FAZER PARTE <br />
+                    DA MAIOR REDE DE <br />
+                    DOAÇÃO</h2>
+                </aside>
+            </header>
+            <Routes>
+                <Route path="/" element={<Inicio />} />
+                <Route path="/livrosdoados" element={<LivrosDoados />} />
+                <Route path="/querodoar" element={<QueroDoar />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
