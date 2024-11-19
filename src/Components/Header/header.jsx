71857f209537b980c6926154c3cd9361511a5054
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-11-18 22:11:45 -0300

    ajuste de imagem

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,25 +1,36 @@
 import S from "./header.module.scss"
 import Livro from "./livro.png"
+import Estudo from "./estudo.png"
 
 function Header() {
     return(
-        <div className={S.inicio}>
-            <div className={S.box1}>
-                <img className={S.livro} src={Livro} alt="imagem de um livro" />
-                <h1 className={S.titulo}>Livros Vai Na Web</h1>
-            </div>
+        <div>
+            <div className={S.inicio}>
+                <div className={S.box1}>
+                    <img className={S.livro} src={Livro} alt="imagem de um livro" />
+                    <h1 className={S.titulo}>Livros Vai Na Web</h1>
+                </div>
 
-            <div className={S.box2}>
-                <li className={S.lista}>
-                    <ol>Início</ol>
-                    <ol>Livros Doados</ol>
-                    <ol>Quero Doar</ol>
-                </li>
+                <div className={S.box2}>
+                    <li className={S.lista}>
+                        <ol>Início</ol>
+                        <ol>Livros Doados</ol>
+                        <ol>Quero Doar</ol>
+                    </li>
+                </div>
+                <div>
+                    <input className={S.input} type="text" placeholder="O que você procura?" />
+                </div>
             </div>
             <div>
-                <input className={S.input} type="text" placeholder="O que você procura?" />
+                <img className={S.estudos} src={Estudo} alt="livros" />
+                <h2>VENHA FAZER PARTE <br />
+                DA MAIOR REDE DE <br />
+                DOAÇÃO</h2>
             </div>
         </div>
+
+
     )
 }
 
