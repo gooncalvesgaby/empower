eb0485e19d9d917436dde93ca990c5ee56eac0a6
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-11-25 17:42:22 -0300

    finalizando footer

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -7,30 +7,34 @@ import Instagram from "./insta.png"
 
 function Footer() {
     return(
-        <footer>
-            <nav>
+        <>
+            <section className={S.boxsection}>
                 <div>
                     <p>4002-8922</p>
                 </div>
                 <div>
                     <i>
-                        <img src={Facebook} alt="imagem do fb" />
+                        <img className={S.icons} src={Facebook} alt="imagem do fb" />
                     </i>
                     <i>
-                        <img src={Twitter} alt="imagem do tt" />
+                        <img className={S.icons} src={Twitter} alt="imagem do tt" />
                     </i>
                     <i>
-                        <img src={Youtube} alt="imagem do yt" />
+                        <img className={S.icons} src={Youtube} alt="imagem do yt" />
                     </i>
                     <i>
-                        <img src={Linkedin} alt="imagem do in" />
+                        <img className={S.icons} src={Linkedin} alt="imagem do in" />
                     </i>
                     <i>
-                        <img src={Instagram} alt="imagem do insta" />
+                        <img className={S.icons} src={Instagram} alt="imagem do insta" />
                     </i>
                 </div>
-            </nav>
-        </footer>
+            </section>
+            <footer className={S.myFooter}>
+                <h5 className={S.texto}>Layout desenvolvido pela Vai
+                 Na Web para fins educativos - 2024</h5>
+            </footer>
+        </>
     )
 }
 
