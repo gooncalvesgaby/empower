5fa0efaa0d9f9ee3e9b63d701944d8a7ab2192d6
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-11-26 12:15:23 -0300

    finalizando ajustes

--- src/Components/Pages/livrosDoados/Doados.jsx
+++ src/Components/Pages/livrosDoados/Doados.jsx
@@ -1,7 +1,20 @@
+import S from "./doados.module.scss"
+import Protagonista from "./protagonista.png"
+
 function Doados() {
     return(
-        <>
-        </>
+        <div className={S.conteudo}>
+            <section>
+                <h1>Livros Doados</h1>
+            </section>
+            <div className={S.infos}>
+                <img className={S.protago} src={Protagonista} alt="imagem de um livro" />
+                <p className={S.text}>O protagonista <br />
+                Susanne Andrade <br />
+                Ficcão
+                </p>
+            </div>
+        </div>
     )
 }
 
